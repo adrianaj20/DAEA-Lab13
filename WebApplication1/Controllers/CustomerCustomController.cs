29933e78d2e812b78ad6e357e569221ced3aed6b
adriannc21 <alexandernajarro17@gmail.com>
2023-12-04 00:38:49 -0500

    final commit

---
+++ WebApplication1/Controllers/CustomerCustomController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomerCustomController : ControllerBase
+    {
+        private readonly MyDataContext _context;
+
+        public CustomerCustomController(MyDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Customer> GetProductsListFilterNameOrLastName(string value)
+        {
+            var response = _context.Customers
+                .Where(x => x.FirstName.Contains(value) || x.LastName.Contains(value))
+                .OrderByDescending(x => x.LastName)
+                .ToList();
+
+            return response;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/InvoiceCustomController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InvoiceCustomController : ControllerBase
+    {
+        private readonly MyDataContext _context;
+
+        public InvoiceCustomController(MyDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Invoice> GetInvoicesByCustomerName(string customerName)
+        {
+            var response = _context.Invoices
+                .Include(x => x.Customer)
+                //.Where(x => x.Customer.FirstName == customerName)
+                .Where(x => x.Customer.FirstName.Contains(customerName))
+                .OrderByDescending(i => i.Customer.FirstName)
+                .ToList();
+
+            return response;
+        }
+    }
+}
